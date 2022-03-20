b0a69c7f6647cf109b89fb83fa2cd783e5a26b25
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-03-20 11:01:07 +0200

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,6 +1,6 @@
 import { request, gql } from 'graphql-request';
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT                                                                                            ;
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT                                                                                          ;
 //  Richard nkulu's work
 export const getPosts = async () => {
     const query = gql`
